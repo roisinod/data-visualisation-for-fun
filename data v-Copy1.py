91afa59fb1d0dbf65c169c3c9c85f8458c4afa80
roisinod <55452431+roisinod@users.noreply.github.com>
2019-09-21 22:57:33 +0100

    Update data v-Copy1.py

--- data v-Copy1.py
+++ data v-Copy1.py
@@ -3,8 +3,8 @@
 
 #This data visualisation was created using data from Beaches.ie.
 #It was inspired by the YouTube tutorial 'Intro to Data Analysis / Visualization with Python, Matplotlib and Pandas | Matplotlib Tutorial' by YouTuber CS Dojo.
-#I was able to create a data model representing reports of e coli and intestinal enterococci present in the 2019 water testing (over a 5 month period)
-#The result is a graph that shows a contrast between e coli reports and intestinal entercocci reports.
+#I was able to create a data model representing reports of e coli and intestinal enterococci present in the 2019 Rocky Bay water samples (over a 5 month period).
+#The result is a graph that shows a contrast between e coli reports and intestinal enterococci reports.
 
 # In[36]:
 
@@ -23,12 +23,12 @@ from matplotlib import pyplot as plt #importing the matplotlib library under the
 ecoli = [-10, 41, 10, -10, 20] #a list of e coli data.
 intestinalenterococci = [1, 6, 4, 2, 15] #a list of interestinal enterococci data.
 plt.plot(ecoli) #plots the e coli data 
-plt.plot(intestinalenterococci) ##plots the intestinal enterococci data 
+plt.plot(intestinalenterococci) #plots the intestinal enterococci data 
 plt.title ('Rocky Bay Beach Water Quality 2019') #titles the graph 'Rocky Bay Beach Water Quality 2019'
 plt.xlabel('intestinal enterococci') #x axis is titled 'intestinal enterococci'
 plt.ylabel('e coli') #y axis represents e coli count
-plt.legend(['E coli reports', 'Intestinal Enterococci reports']) #creates a legend/key that show what the yellow and blue lines mean
-plt.show() #produces graph
+plt.legend(['E coli reports', 'Intestinal Enterococci reports']) #creates a legend/key that shows what the yellow and blue lines mean
+plt.show() #produces a graph
 
 
 # In[ ]:
